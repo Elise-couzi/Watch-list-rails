88c038e6131359b4fee8635ee95b03cea39b3ad8
elisecouzi <elisecouzi@hotmail.com>
2021-06-10 16:35:16 +0200

    work in progress : views

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,29 @@
 class BookmarksController < ApplicationController
+  def new
+    raise
+    @bookmark = List.find(params[:id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.save
+    if @bookmark.valid?
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
 end
